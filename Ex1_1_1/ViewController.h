bf87537609f8c6d8298ce5ddc37e3f7e2ac321fa
steve0503 <leehr76@gmail.com>
2014-01-09 10:30:36 +0900

    Initial Commit

---
+++ Ex1_1_1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ex1_1_1
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ex1_1_1Tests/Ex1_1_1Tests.m
@@ -0,0 +1,34 @@
+//
+//  Ex1_1_1Tests.m
+//  Ex1_1_1Tests
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex1_1_1Tests : XCTestCase
+
+@end
+
+@implementation Ex1_1_1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
